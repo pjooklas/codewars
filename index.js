bb4547c638f18b7384ae4d7d27b157f6ab41170c
pjooklas <pjooklas@users.noreply.github.com>
2022-03-20 20:17:59 +0200

    String repeat

--- index.js
+++ index.js
@@ -1,15 +1,23 @@
 // https://www.codewars.com/kata/54ff3102c1bad923760001f3
+// Vowels Count
 
 function getCount(str) {
     var vowelsCount = 0;
-
     for (let i = 0; i < str.length; i++) {
         str[i] == 'a' || str[i] == 'e' || str[i] == 'i' || str[i] == 'o' || str[i] == 'u' ?
             vowelsCount++ :
             vowelsCount;
     };
     console.log(vowelsCount);
-
 }
-
 getCount('abracadabra');
+
+////////////////////////////////////////////////////////////////////
+
+// https://www.codewars.com/kata/57a0e5c372292dd76d000d7e
+// String repeat
+
+function repeatStr(n, s) {
+    console.log(s.repeat(n));
+}
+repeatStr(2, "ha ");
