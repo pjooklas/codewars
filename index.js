d536121a20003266c0cd3a35aded4b14ca16a05b
pjooklas <pjooklas@users.noreply.github.com>
2022-03-20 20:02:55 +0200

    Vowel Count

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
-console.log('aa');
-console.log('aa');
+// https://www.codewars.com/kata/54ff3102c1bad923760001f3
+
+function getCount(str) {
+    var vowelsCount = 0;
+
+    for (let i = 0; i < str.length; i++) {
+        str[i] == 'a' || str[i] == 'e' || str[i] == 'i' || str[i] == 'o' || str[i] == 'u' ?
+            vowelsCount++ :
+            vowelsCount;
+    };
+    console.log(vowelsCount);
+
+}
+
+getCount('abracadabra');
