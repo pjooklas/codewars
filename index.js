67216e7059e785d2efd32b39fd401687e2559e3f
pjooklas <pjooklas@users.noreply.github.com>
2022-05-08 21:41:37 +0300

    Moving Zeros To The End

--- index.js
+++ index.js
@@ -571,3 +571,29 @@ function pigIt(str) {
 }
 
 console.log(pigIt('Pig latin is cool')); //'igPay atinlay siay oolcay'
+
+
+/////////////////// Moving Zeros To The End ///////////////////////////////////
+// https://www.codewars.com/kata/52597aa56021e91c93000cb0
+console.log('---');
+
+function moveZeros(arr) {
+    let resultArra = [];
+    let countZeros = 0;
+
+    for (const element of arr) {
+        if (element !== 0) {
+            resultArra.push(element);
+        } else {
+            countZeros++;
+        }
+    }
+
+    for (let i = 0; i < countZeros; i++) {
+        resultArra.push(0);
+    }
+
+    return resultArra;
+}
+
+console.log(moveZeros([false, 1, 0, 1, 2, 0, 1, 3, "a"])); // [false,1,1,2,1,3,"a",0,0]
