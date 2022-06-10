de3d4bad620ac7358387dad4c4db65e17e8edb8a
pjooklas <pjooklas@users.noreply.github.com>
2022-06-10 21:30:32 +0300

    Calculate BMI

--- index.js
+++ index.js
@@ -605,7 +605,21 @@ console.log('---------------');
 
 function removeExclamationMarks(s) {
     return s.split("!").join("");
-  }
+}
 
 console.log(removeExclamationMarks("Hello World!!"));
 
+
+/////////////////// Calculate BMI ///////////////////////////////////
+// https://www.codewars.com/kata/57a429e253ba3381850000fb
+console.log('---------------');
+
+function bmi(weight, height) {
+    let bmi= weight / (height**2);
+    return bmi<=18.5 ? "Underweight" :
+            bmi<=25 ? "Normal" :
+            bmi<=30 ? "Overweight" :
+            "Obese"
+  }
+
+console.log(bmi(80, 1.80)); //normal
